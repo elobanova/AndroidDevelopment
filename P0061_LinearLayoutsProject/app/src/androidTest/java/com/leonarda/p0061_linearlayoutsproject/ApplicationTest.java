532e0fe02557487fd3af16e3c4d3dee8f021f028
kate <lobanova.e.o@gmail.com>
2015-03-07 19:47:44 +0100

    Combining horizontal and vertical linear layouts

---
+++ P0061_LinearLayoutsProject/app/src/androidTest/java/com/leonarda/p0061_linearlayoutsproject/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.leonarda.p0061_linearlayoutsproject;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
