2d89e9d9507815fb5ef4eb696bfa02c810170ee1
kate <lobanova.e.o@gmail.com>
2015-04-29 21:17:53 +0200

    Added listview and a small info dialog

---
+++ MensaViewer/app/src/main/java/rwth/lab/android/mensaviewer/InformationDialogFragment.java
@@ -0,0 +1,36 @@
+package rwth.lab.android.mensaviewer;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+public class InformationDialogFragment extends DialogFragment {
+    public static InformationDialogFragment newInstance() {
+        return new InformationDialogFragment();
+    }
+
+    // Build AlertDialog using AlertDialog.Builder
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        return new AlertDialog.Builder(getActivity())
+                .setMessage(R.string.message)
+
+                        // User cannot dismiss dialog by hitting back button
+                .setCancelable(false)
+
+                        // Set up browse Button
+                .setPositiveButton(R.string.visitmensa,
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(final DialogInterface dialog,
+                                                int id) {
+                                ((ViewerInitialActivity) getActivity())
+                                        .startImplicitActivation();
+                            }
+                        })
+
+                        // Set up Not now Button
+                .setNegativeButton(R.string.notnow, null).create();
+    }
+}
--- MensaViewer/app/src/main/java/rwth/lab/android/mensaviewer/ViewerInitialActivity.java
+++ MensaViewer/app/src/main/java/rwth/lab/android/mensaviewer/ViewerInitialActivity.java
@@ -1,39 +1,89 @@
 package rwth.lab.android.mensaviewer;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.DialogFragment;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
-public class ViewerInitialActivity extends ActionBarActivity {
+public class ViewerInitialActivity extends ListActivity {
+
+    private static final String[] LIST_OF_MENSAS = {"Mensa Ahornstraße", "Mensa Vita"};
+    private static final int MENU_INFORMATION = Menu.FIRST;
+    private static final String TAG = "Mensa-Viewer";
+    private static final String URL = "http://www.studentenwerk-aachen.de/de/gastronomie/speiseplaene.html";
+
+    private DialogFragment mDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_viewer_initial);
-    }
+        // Create a new Adapter containing a list of mensas
+        // Set the adapter on this ListActivity's built-in ListView
+        setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item,
+                LIST_OF_MENSAS));
 
+        ListView lv = getListView();
+
+        // Enable filtering when the user types in the virtual keyboard
+        lv.setTextFilterEnabled(true);
+
+        // Set an setOnItemClickListener on the ListView
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                // TODO: Call HTTP GET
+                Toast.makeText(getApplicationContext(),
+                        ((TextView) view).getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_viewer_initial, menu);
+        super.onCreateOptionsMenu(menu);
+
+        menu.add(Menu.NONE, MENU_INFORMATION, Menu.NONE, R.string.information);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case MENU_INFORMATION:
+                showDialogFragment();
+                return true;
+            default:
+                return false;
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    private void showDialogFragment() {
+        mDialog = InformationDialogFragment.newInstance();
+        mDialog.show(getFragmentManager(), "Alert");
+    }
+
+    /**
+     * Start a Browser Activity to view a web page or its URL.
+     */
+    public void startImplicitActivation() {
+
+        Log.i(TAG, "Entered startImplicitActivation()");
+
+        // Create a base intent for viewing a URL
+        Uri webpage = Uri.parse(URL);
+        Intent baseIntent = new Intent(Intent.ACTION_VIEW, webpage);
+        startActivity(baseIntent);
     }
 }
