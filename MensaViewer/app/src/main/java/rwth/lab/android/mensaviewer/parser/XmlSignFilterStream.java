41dc4d109dfdd7981732e573b64dc3795b086df3
kate <lobanova.e.o@gmail.com>
2015-05-01 17:24:15 +0200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    
    	MensaViewer/app/src/main/java/rwth/lab/android/mensaviewer/http/WeekPlanGetRequest.java
    
    	MensaViewer/app/src/main/java/rwth/lab/android/mensaviewer/parser/WeekPlanXmlPullParser.java

--- MensaViewer/app/src/main/java/rwth/lab/android/mensaviewer/parser/XmlSignFilterStream.java
+++ MensaViewer/app/src/main/java/rwth/lab/android/mensaviewer/parser/XmlSignFilterStream.java
@@ -1,6 +1,5 @@
 package rwth.lab.android.mensaviewer.parser;
 
-import java.io.FilterInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayDeque;
@@ -8,7 +7,7 @@ import java.util.Deque;
 
 /**
  * Created by evgenijavstein on 30/04/15.
- *
+ * <p/>
  * Helper class to get rid of & signs which
  * makes the XMLPullParser fails, since & signs should be
  * encoded as &amp;
@@ -20,8 +19,6 @@ public class XmlSignFilterStream extends InputStream {
     private final byte[] readBuf = new byte[REPLACEMENT.length];
     private final Deque<Byte> backBuf = new ArrayDeque<Byte>();
     private final InputStream in;
-
-
 
     public XmlSignFilterStream(InputStream in) {
         this.in = in;
@@ -42,19 +39,18 @@ public class XmlSignFilterStream extends InputStream {
     private void peekAndReplace() throws IOException {
         int read = super.read(readBuf, 0, REPLACEMENT.length);
         for (int i1 = read - 1; i1 >= 0; i1--) {
-            backBuf.push(readBuf[i1]);//first take letters which you would loose else, by just replacing & with amp;
+            backBuf.push(readBuf[i1]); //first take letters which you would loose else, by just replacing & with amp;
         }
         for (int i = 0; i < REPLACEMENT.length; i++) {
             if (read != REPLACEMENT.length || readBuf[i] != REPLACEMENT[i]) {
                 for (int j = REPLACEMENT.length - 1; j >= 0; j--) {
                     // In reverse order
-                    backBuf.push(REPLACEMENT[j]);//then appends replacement "amp;" letters
+                    backBuf.push(REPLACEMENT[j]); //then appends replacement "amp;" letters
                 }
                 return;
             }
         }
     }
-
 }
 
 
